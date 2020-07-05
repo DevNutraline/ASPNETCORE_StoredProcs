8be99b763ac309cbbadf5148f1204e583aa5cb68
Nutraline Limitada <63253310+DevNutraline@users.noreply.github.com>
2020-07-04 23:57:06 -0400

    Cambiando a versión  Net Core 3.1

--- ASPNETCore_StoredProcs/Data/ValuesRepository.cs
+++ ASPNETCore_StoredProcs/Data/ValuesRepository.cs
@@ -1,10 +1,7 @@
 ﻿using ASPNETCore_StoredProcs.Models;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
-using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace ASPNETCore_StoredProcs.Data
--- ASPNETCore_StoredProcs/Startup.cs
+++ ASPNETCore_StoredProcs/Startup.cs
@@ -6,11 +6,11 @@ using ASPNETCore_StoredProcs.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Hosting;
 
 namespace ASPNETCore_StoredProcs
 {
@@ -27,11 +27,12 @@ namespace ASPNETCore_StoredProcs
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<ValuesRepository>();
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddControllers();
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -44,7 +45,13 @@ namespace ASPNETCore_StoredProcs
             }
 
             app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
         }
     }
 }
